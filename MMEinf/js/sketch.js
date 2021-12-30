8603fe5b6749da78c69b438fd3aa207c1834c8e9
Paul Eybe <eybepaul@gmail.com>
2021-12-30 16:49:25 +0100

    Update sketch.js & drawing.html

--- MMEinf/js/sketch.js
+++ MMEinf/js/sketch.js
@@ -1,163 +1,112 @@
-let brushButton, eraserButton, clearButton, lineButton, sizeSlider, colorPicker, undoButton, on;
-on = false;
+let brushButton, eraserButton, clearButton, lineButton, sizeSlider, colorPicker, undoButton, pickButton, redoButton;
+let strokeColor = 'black';
+
 
 function setup() {
-    var height = document.getElementById("myCanvas").clientHeight;
-    var width = document.getElementById("myCanvas").clientWidth;
+    let height = document.getElementById("myCanvas").clientHeight;
+    let width = document.getElementById("myCanvas").clientWidth;
 
-    alert("height/width " +height +"/" + width);
-    var canvas = createCanvas(width, height);
+    let canvas = createCanvas(width, height);
     canvas.parent('myCanvas');
-    background(255,255,255);
+    background(255);
 
     brushButton = createButton('brush');
     brushButton.parent('brushBtn');
-    //brushButton.position(10, 460);
     brushButton.mousePressed(noErase);
-   // brushButton.mousePressed(setBoolFalse);
 
     eraserButton = createButton('erase');
     eraserButton.parent('eraseBtn');
-    //eraserButton.position(60, 460);
     eraserButton.mousePressed(erase);
 
     clearButton = createButton('clear');
-    //clearButton.position(110, 460);
     clearButton.parent('clearBtn');
     clearButton.mousePressed(clearBG);
 
     undoButton = createButton('undo');
-    //undoButton.position(160, 460);
     undoButton.parent('undoBtn');
-    undoButton.mousePressed(undoToPreviousState);
+    undoButton.mousePressed(undoToPrevState);
 
-    // lineButton = createButton('line');
-    // lineButton.position(210, 460);
-    // // lineButton.mousePressed(drawLine);
-    // // lineButton.mouseReleased(endLine);
-    // // lineButton.mousePressed(setBoolTrue);
+    redoButton = createButton('redo');
+    redoButton.parent('redoBtn');
+    redoButton.mousePressed(redoPrevState);
 
     sizeSlider = createSlider(1, 50, 10);
-    //sizeSlider.position(260, 460);
     sizeSlider.parent('sliderBtn');
     sizeSlider.style('width', '100px');
 
     colorPicker = createColorPicker('black');
     colorPicker.parent('colorpickerBtn');
-    //colorPicker.position(380, 460);
 
     saveState();
 }
 
 function draw() {
-    if (!on) {
-        if(mouseIsPressed && pmouseY <= 450){
-            line(mouseX, mouseY, pmouseX, pmouseY);
-        }
-
-        let val = sizeSlider.value();
-        strokeWeight(val);
-        stroke(colorPicker.color());
+    //TODO remove if-condition for html integration?
+    if(mouseIsPressed && pmouseY <= 450){
+        line(mouseX, mouseY, pmouseX, pmouseY);
     }
 
-    if(on) {
-        //TODO: draw line
-    }
-
+    strokeWeight(sizeSlider.value());
+    stroke(colorPicker.color());
 }
 
 function clearBG() {
     background(255);
 }
 
-let valX1, valX2, valY1, valY2;
-
-function drawLine(){
-    if (mouseIsPressed){
-        valX1 = mouseX;
-        valY1 = mouseY;
-    }
-}
-
-function endLine(){
-    valX2 = mouseX;
-    valY2 = mouseY;
-}
-
-function setBoolTrue(){
-    on = true;
-}
-
-function setBoolFalse(){
-    on = false;
-}
-
+//TODO UNDO/REDO
+const previousStates = [];
+let redoState = null;
+let redone = false;
 
 function keyPressed(e) {
-    // check if the event parameter (e) has Z (keycode 90) and ctrl or cmnd
-    if (e.keyCode == 90 && (e.ctrlKey || e.metaKey)) {
-        undoToPreviousState();
+    //TODO shift+strg+z redo?
+
+    // if (e.keyCode === 90 && (e.ctrlKey || e.metaKey) && (e.shiftKey)) {
+    //     redoToPreviousState();
+    // }
+
+    if (e.keyCode === 90 && (e.ctrlKey || e.metaKey)) {
+        undoToPrevState();
     }
 }
 
-function undoToPreviousState() {
-    // if previousState doesn't exist ie is null
-    // return without doing anything
-    if (!previousState) {
-        return;
-    }
-    // else draw the background (in this case white)
-    // and draw the previous state
-    background(255);
-    image(previousState, 0, 0);
-    // then set previous state to null
-    previousState = null;
+function saveState() {
+    previousStates.push(get());
 }
 
 function mousePressed() {
-    // the moment input is detect save the state
-    saveState();
+    //TODO remove if-condition for html integration?
+    if (pmouseY < 400){
+        saveState();
+    }
 }
 
-// function mousePressed() {
-//     loadPixels();
-//
-//     let r = pixels[(mouseY * width + mouseX) * 4];
-//     let g = pixels[(mouseY * width + mouseX) * 4 + 1];
-//     let b = pixels[(mouseY * width + mouseX) * 4 + 2];
-//     console.log(r,g,b)
-//     bucket(mouseX, mouseY, 0, random(255), random(255), random(255), r, g, b);
-//     updatePixels();
-// }
+function undoToPrevState() {
+    if (previousStates[previousStates.length-1] == null) {
+        return;
+    }
+    redone = false;
+    redoState = get();
+    background(255);
+    image(previousStates[previousStates.length-1], 0, 0);
+    previousStates.splice(previousStates.length-1, 1);
 
-function saveState() {
-    // save state by taking image of background
-    // for more info look at reference for get
-    previousState = get();
 }
 
-//
-// function bucket(x, y, ii, R, G, B, ro, go, bo) {
-//     if(ii > 1000)return;
-//     let r = pixels[(y * width + x) * 4];
-//     let g = pixels[(y * width + x) * 4 + 1];
-//     let b = pixels[(y * width + x) * 4 + 2];
-//     // console.log(r,g,b,ro,go,bo)
-//     if (r == ro && g == go && b == bo) {
-//         pixels[(y * width + x) * 4] = R;
-//         pixels[(y * width + x) * 4+1] = G;
-//         pixels[(y * width + x) * 4+2] = B;
-//         for (let i = -1; i <= 1; i++) {
-//             if (i == 0) continue;
-//             if (x + i >= width) break;
-//             if (x + i < 0) break;
-//             bucket(x + i, y, ii+1, R, G, B, ro, go, bo);
-//         }
-//         for (let i = -1; i <= 1; i++) {
-//             if (i == 0) continue;
-//             if (y + i >= width) break;
-//             if (y + i < 0) break;
-//             bucket(x, y + i, ii+1, R, G, B, ro, go, bo);
-//         }
-//     }
-// }
+//TODO make multiple states redoable?
+function redoPrevState() {
+    if (redoState == null) {
+        return;
+    }
+    if (!redone) {
+        saveState();
+        redone = true;
+    }
+    background(255);
+    image(redoState, 0, 0);
+}
+
+//TODO saveCanvas()?
+
+
